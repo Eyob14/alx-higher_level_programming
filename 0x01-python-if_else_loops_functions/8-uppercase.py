630b6638b409fea97de7d0e073c27e576d79c2bf
Eyob14 <eyobzebene3580@gmial.com>
2021-11-16 13:07:23 -0800

    updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin python3
 def uppercase(str):
     word = ""
     for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+        if (ord(i) >= ord('a')) and (ord(i) <= ord('z')):
             word += chr(ord(i) - 32)
         else:
             word += i
