853777f5fbe4209a45ce219861477c91253992af
Eyob14 <eyobzebene3580@gmial.com>
2021-11-22 13:49:48 -0800

    divisible

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,8 @@ def max_integer(my_list=[]):
     maximum = my_list[0]
     if len(my_list) == 0:
         return None
-    for ele in my_list:
-        if maximum < ele:
-            maximum = ele
-    return maximum
+    else:
+        for ele in my_list:
+            if maximum < ele:
+                maximum = ele
+        return maximum
