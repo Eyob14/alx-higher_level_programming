60dc6e5cc108883f94d248ead09eeca9d7e165a5
Eyob14 <eyobzebene3580@gmial.com>
2021-11-22 11:22:13 -0800

    lists of lists

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{}".format(matrix[row][col]), end=" ")
+        print()
