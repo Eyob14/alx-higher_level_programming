0adc3e8010140377760bcf78d628e54f698ae672
Eyob14 <eyobzebene3580@gmial.com>
2021-11-22 13:26:27 -0800

    tuple add

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for col in range(len(matrix[row])):
-            print("{}".format(matrix[row][col]), end=" ")
-        print()
+    for elm in matrix:
+        print(" ".join("{:d}".format(i) for i in elm))
