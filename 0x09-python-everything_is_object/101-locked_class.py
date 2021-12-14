81de49f264d3c4c720c1f6524ce88f0d807f2001
Eyob <86522632+Eyob14@users.noreply.github.com>
2021-12-14 22:04:41 +0300

    locked class updated

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-    """
-    Definition for a LockedClass class.
-    """
-
-
-    class LockedClass:
-        """LockedClass class that defines 1 attribute"""
-
-        __slots__ = ['first_name']
+class LockedClass:
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ['first_name']
