bf1b4ec007b00015b70874427f6c4c408544d0d4
Eyob <86522632+Eyob14@users.noreply.github.com>
2022-02-21 00:35:06 +0300

    new approach

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord("a"), ord("z")+1):
-    if (i!=ord("q")) and (i!=ord("e")):
+for i in range(ord('a'), ord('z')+1):
+    if (chr(i) != 'e') and (chr(i) != 'q'):
         print("{}".format(chr(i)), end="")
