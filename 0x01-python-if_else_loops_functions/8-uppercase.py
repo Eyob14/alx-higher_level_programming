4ed952b3354083405cad1af436bad4377cfcf171
Eyob <86522632+Eyob14@users.noreply.github.com>
2022-02-21 02:02:18 +0300

    update 8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     result = ""
     for letter in str:
-        if ord(letter) >= ord('a') and ord(letter) <= ord('z'): 
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
             result += chr(ord(letter) - (ord('a') - ord('A')))
         else:
             result += letter
