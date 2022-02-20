2a489e06ded3271376172bf9c95b2ee4288ef928
Eyob <86522632+Eyob14@users.noreply.github.com>
2022-02-21 01:35:29 +0300

    update 6-print_comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(8):
     for j in range(i, 10):
         if i != j:
-            print("{}{}, ".format(i,j), end="")
+            print("{}{}, ".format(i, j), end="")
 print("{}{}".format(i+1, j))
