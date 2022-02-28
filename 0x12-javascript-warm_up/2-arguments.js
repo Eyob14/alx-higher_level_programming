498f73fb6d903db620668f53dd268b012e2e9a90
Eyob14 <eyobzebene3580@gmial.com>
2022-02-28 13:58:51 -0800

    multi_languages updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-const argv = require('process').argv
-
-if (argv.length == 2) {
-    console.log('No argument');
+const argv = require('process').argv;
+const len = argv.length;
+if (len === 2) {
+  console.log('No argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (argv.length == 3) {
-    console.log('Argument found');
-}
-else {
-    console.log('Arguments found');
-}
-
-	 
