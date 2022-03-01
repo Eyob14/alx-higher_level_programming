a9925a41f64d7c640903790145c42550976a9664
Eyob14 <eyobzebene3580@gmial.com>
2022-02-28 16:03:08 -0800

    errors fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-'use strict';
-let nextMax = 0;
-const args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  nextMax = args[args.length - 2];
-}
-console.log(nextMax);
+console.log(process.argv.length < 4 ? 0 : process.argv.slice(2).sort((a, b) => b - a)[1]);
