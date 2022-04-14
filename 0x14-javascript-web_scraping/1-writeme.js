faa3cccf6b662524e48577ddcf892bf13bbe78c0
Eyob14 <eyobzebene3580@gmail.com>
2022-04-14 13:06:07 +0000

    1-writeme.js updated again

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,12 +2,8 @@
 const fs = require('fs');
 const file = process.argv[2];
 const word = process.argv[3];
-if (file && word) {
-  fs.writeFile(file, word, 'utf-8', err => {
-    if (err) {
-      console.error(err);
-    }
-  });
-} else {
-  throw new Error('Enter a content to write');
-}
+fs.writeFile(file, word, 'utf-8', err => {
+  if (err) {
+    console.error(err);
+  }
+});
