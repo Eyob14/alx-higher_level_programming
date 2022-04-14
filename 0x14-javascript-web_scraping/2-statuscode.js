0ef81fe49f6632bfb59cf96cd27d67353940457c
Eyob14 <eyobzebene3580@gmail.com>
2022-04-14 13:14:28 +0000

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (err, res) => {
+  if (!err) {
+    console.log('code: ', res && res.statusCode);
+  }
+});
